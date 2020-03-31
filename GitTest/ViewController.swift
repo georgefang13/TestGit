a5a101635a70e28a274352f5970a1e0afe21462c
georgefang13 <geofang10@gmail.com>
2020-03-31 09:11:54 -0600

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Fang, George - Student on 3/31/20.
+//  Copyright © 2020 Fang, George - Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
