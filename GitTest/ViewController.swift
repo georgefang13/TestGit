e4f83c9e3afa25aa3641c075921a2369a6782f68
georgefang13 <geofang10@gmail.com>
2020-03-31 09:23:32 -0600

    Connected button to an action that changed label

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func helloButton(_ sender: Any) {
+        helloLabel.text = "Hello"
+    }
 }
 
